1963d69e346b29d7ba69f3c30adad2cb6de7da18
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-07 02:36:18 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+def my_collect(array)
+  if block_given?
+    return_array = []
+    counter = 0 
+    if counter == array.length 
+      return return_array
+    else
+      while counter < array.length
+        return_array.push(yield (array[counter]))
+        counter += 1 
+      end
+      return return_array
+    end
+  else 
+    return nil 
+  end   
+end 
 
 
